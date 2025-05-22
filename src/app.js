8d9e3d21954230de208af8c6fca69ad051ce1221
Gabriel Saraiva <gabriel.saraiva.flooxai@gmail.com>
2025-05-22 17:52:27 -0300

    refactor: centralize server setup and add CORS
    - Move server initialization and routes to app.js
    - Simplify loader.js to only import dependencies
    - Add new CORS middleware for cross-origin requests
    - Include test route in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
+const port = 3003
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./middlewares/cors')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// Middlewares
+app.use(allowCors)
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('API HelpHeal está funcionando!')
+})
+
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`)
+})
 
 module.exports = app
--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,2 @@
 const app = require('./app')
 const db = require('./config/database')
-
-const port = 3003
-app.listen(port, () => {
-  console.log(`Backend is running on port ${port}`)
-})
