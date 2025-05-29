26855ff13901c3638428ad91b4224ad28956e10d
Gabriel Saraiva <gabriel.saraiva.flooxai@gmail.com>
2025-05-29 01:31:03 -0300

    chore(cors): adjust headers for Vercel frontend compatibility

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,14 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
+  const allowedOrigin = 'https://helpheal-frontend.vercel.app'
+
+  res.header('Access-Control-Allow-Origin', allowedOrigin)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Credentials', 'true')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
 }
